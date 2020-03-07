064f77855544750ae7c3eaf688eb74b09b4b6182
Igor Tikhanovich <tikhanovich.89@gmail.com>
2020-03-07 23:59:46 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import 'bootstrap';
+import 'owl.carousel';
+import 'magnific-popup';
+import 'inputmask/dist/jquery.inputmask.min';
+
+import WOW from 'wowjs';
+window.wow = new WOW.WOW({ live: false, mobile: false });
+window.wow.init();
+
+import './js/scripts';
+
+import 'bootstrap/scss/bootstrap.scss';
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+import 'magnific-popup/dist/magnific-popup.css';
+import 'animate.css';
+import './scss/style.scss';
+import './scss/responsive.scss';
---
+++ src/js/scripts.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+    $(window).on('load scroll', function(e) {
+        var scroll = document.documentElement.scrollTop;
+        if (scroll == 0) scroll = window.pageYOffset;
+
+        if (scroll > 100) {
+            $('header').addClass('fixed');
+        } else {
+            $('header').removeClass('fixed');
+        }
+    });
+
+    $('input[name="phone"]').inputmask('+375 (99) 999-99-99');
+
+    $('.owl-slider').owlCarousel({
+        loop: true,
+        items: 1,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplayTimeout: 5000,
+        onInitialized: dotNumbers,
+        onRefreshed: sliderAnimation,
+        onTranslated: sliderAnimation
+    });
+
+    $('body').on('click', '.toggle-contact', function(e) {
+        e.preventDefault();
+
+        $('.contact').toggleClass('active');
+    });
+
+    $(document).mouseup(function(e) {
+        var el = $('header .contact');
+        if (
+            !el.is(e.target) &&
+            el.has(e.target).length === 0 &&
+            !$(e.target).hasClass('toggle-contact')
+        ) {
+            el.removeClass('active');
+        }
+    });
+
+    $('.popup-with-form').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#name',
+        callbacks: {
+            beforeOpen: function() {
+                if ($(window).width() < 768) {
+                    this.st.focus = false;
+                } else {
+                    this.st.focus = '#name';
+                }
+            }
+        }
+    });
+});
+
+$(document).on('af_complete', function(event, response) {
+    if (response.success) {
+        $.magnificPopup.close();
+    }
+});
+
+function sliderAnimation(e) {
+    $(e.target)
+        .find('.owl-item:not(.active) .animated')
+        .each(function() {
+            var classname = $(this).data('animation');
+            $(this).removeClass(classname);
+        });
+
+    $(e.target)
+        .find('.owl-item.active .animated')
+        .each(function() {
+            var classname = $(this).data('animation');
+            $(this).addClass(classname);
+        });
+}
+
+function dotNumbers(e) {
+    $(e.target)
+        .find('.owl-dot')
+        .each(function() {
+            $(this)
+                .find('span')
+                .text('0' + ($(this).index() + 1));
+        });
+}
